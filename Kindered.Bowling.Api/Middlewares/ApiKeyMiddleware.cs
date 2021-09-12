080b93457c37b9e079be55ddfc28ce4a4c205778
Abinash Manandhar <abinash.mdr@gmail.com>
2021-09-12 16:54:51 +1000

    Complete ApiKeyMiddleware implementation and add API versioning and Swashbuckle support. - Configure API Versioning - Configure Swagger documentation with xml documentation support - Configure Swagger UI with API Key support - Configure ApiKeyMiddleware - Add ApiKey config to app.config

--- Kindered.Bowling.Api/Middlewares/ApiKeyMiddleware.cs
+++ Kindered.Bowling.Api/Middlewares/ApiKeyMiddleware.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Threading.Tasks;
 
 namespace Kindred.Bowling.Api.Middlewares
@@ -9,17 +8,59 @@ namespace Kindred.Bowling.Api.Middlewares
     /// <summary>
     /// Middleware to handle API key authentication
     /// Expects API Key in ApiKey header
+    /// Based on: https://github.com/aram87/SecuringWebApiUsingApiKey
     /// </summary>
     public class ApiKeyMiddleware
     {
+        private readonly RequestDelegate _next;
+        private const string ApiKeyHeaderName = "ApiKey";
+        private readonly ILogger<ApiKeyMiddleware> _logger;
+        private readonly IConfiguration _configuration;
+
+        /// <summary>
+        /// Creates an instance of the ApiMiddleware Class
+        /// </summary>
+        /// <param name="next">The RequestDelegate to build the request pipeline</param>
+        /// <param name="logger">The configured Logger</param>
+        /// <param name="configuration">Application Configuration</param>
         public ApiKeyMiddleware(RequestDelegate next, ILogger<ApiKeyMiddleware> logger, IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _next = next;
+            _logger = logger;
+            _configuration = configuration;
         }
 
+        /// <summary>
+        /// Validates API key in the request header to API key in the app configuration file
+        /// </summary>
+        /// <param name="context">The HttpContext for the request</param>
+        /// <returns>
+        /// Task that represents the execution of the middleware with
+        /// HTTP 401 - Unauthorized response if API key is not provided or is invalid
+        /// or continues the execution of request pipeline
+        /// </returns>
         public async Task InvokeAsync(HttpContext context)
         {
-            throw new NotImplementedException();
+            if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("API Key is required to authorize.");
+                _logger.LogDebug("API Key not provided.");
+                return;
+            }
+
+            var apiKey = _configuration.GetValue<string>(ApiKeyHeaderName);
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Unauthorized client - invalid API Key.");
+                _logger.LogDebug("Unauthorized client - invalid API Key.");
+                return;
+            }
+
+            _logger.LogDebug("Client authorised with API Key auth.");
+            await _next(context);
         }
     }
 }
--- Kindered.Bowling.Api/Startup.cs
+++ Kindered.Bowling.Api/Startup.cs
@@ -1,10 +1,16 @@
+using Kindred.Bowling.Api.Middlewares;
 using Kindred.Bowling.Api.Services.Framing;
 using Kindred.Bowling.Api.Services.Scoring;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace Kindred.Bowling.Api
 {
@@ -25,6 +31,54 @@ namespace Kindred.Bowling.Api
             // register custom services
             services.AddScoped<IScoringService, TraditionalScoringService>();
             services.AddScoped<IFramingService, FramingService>();
+
+            // Register the Swagger generator
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Kindred.Bowling.Api",
+                    Description = "API for Bowling Score Calculation",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Kindred",
+                        Email = "test-emai@domain.net"
+                    }
+                });
+
+                // Add API key auth to swagger
+                c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme()
+                {
+                    Type = SecuritySchemeType.ApiKey,
+                    In = ParameterLocation.Header,
+                    Name = "ApiKey",
+                    Description = "API Key auth",
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "ApiKey" }
+                        },
+                        new string[] { }
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            // Add support for ApiVersioning
+            services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +88,20 @@ namespace Kindred.Bowling.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Kindred.Bowling.Api.V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            // Use ApiKeyMiddleware for APIKey authentication 
+            app.UseMiddleware<ApiKeyMiddleware>();
 
             app.UseHttpsRedirection();
 
